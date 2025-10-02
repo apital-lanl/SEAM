a63895e36458e7c2274066b3d4181c2a485e4966
apital-lanl <acpital@lanl.gov>
2025-10-02 12:12:47 -0600

    Added license for open-source release

--- Viewer.py
+++ Viewer.py
@@ -1,11 +1,22 @@
 
 """
-Created:  2025-07-18
-Modified: 2025-07-18
+© 2025. Triad National Security, LLC. All rights reserved.
+This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos National 
+Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S. Department of 
+Energy/National Nuclear Security Administration. All rights in the program are reserved by Triad 
+National Security, LLC, and the U.S. Department of Energy/National Nuclear Security Administration. 
+The Government is granted for itself and others acting on its behalf a nonexclusive, paid-up, 
+irrevocable worldwide license in this material to reproduce, prepare. derivative works, distribute 
+copies to the public, perform publicly and display publicly, and to permit others to do so.
 
-@author: Aaron Pital
+@author: Aaron Pital (Los Alamos National Lab)
+Created:  2024-04-19
 
-Description: Renamed from 'SH2D' to 'SpatialArray' 2024-09-13
+Description: 
+
+
+Notes:
+- Renamed from 'SH2D' to 'SpatialArray' 2024-09-13
 """
 
 import tkinter as tk
